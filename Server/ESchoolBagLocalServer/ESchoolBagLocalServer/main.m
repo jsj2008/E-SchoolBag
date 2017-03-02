e8bc2ea8354ed4996b91ca181e925cf01b677493
23 <a23@23deiMac.local>
2017-03-02 11:53:09 +0800

    init

---
+++ Server/ESchoolBagLocalServer/ESchoolBagLocalServer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ESchoolBagLocalServer
+//
+//  Created by 23 on 2017/3/2.
+//  Copyright © 2017年 23. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
